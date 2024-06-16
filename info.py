339deed08f6dff6f8667f87ab081845a42ee7884
AK BOTS DEVELOPER <108203396+Aneebrahman@users.noreply.github.com>
2024-06-16 14:12:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://A:<A>@cluster0.nu8uuof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://A:<a>@cluster0.nu8uuof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
