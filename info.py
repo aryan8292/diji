e05a71541c16831436e2a3019f75276783eda2a6
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2024-12-22 02:29:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Aryan6969:Aryan6969@cluster0.krhmwhe.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://unknown4u1082:ZqSpbcQd0oXD1pS0@cluster0.vucwc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'aryan')
 
